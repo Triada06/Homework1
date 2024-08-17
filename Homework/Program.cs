67e892223a793ff07dec836d8ce9fb40474358aa
Nizami <tnizami006@gmail.com>
2024-08-17 18:36:20 +0400

    Files have been uploaded

---
+++ Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using Homework;
+
+
+User user = new User();
+
+Console.WriteLine("Enter username: ");
+user.UserName = Console.ReadLine();
+
+Console.WriteLine("Enter password: ");
+user.Password = Console.ReadLine();
+
+Console.WriteLine(user.UserName + user.Password);
+
+
+Console.WriteLine(user.HasUpper("Salam"));
---
+++ Homework/User.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace Homework
+{
+    internal class User
+    {
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set 
+            {
+                if (value.Length > 6 && value.Length < 25) { userName = value; }
+            }
+        }
+
+        private string  password;
+
+        public string  Password
+        {
+            get { return password; }
+            set 
+            {
+                if (value.Length > 8 && value.Length < 25)
+                {
+                    bool hasUpperCase = false;
+                    bool hasLowerCase = false;
+
+                    foreach (char c in value)
+                    {
+                        if (c >= 65 && c <= 90) hasUpperCase = true;
+                            
+                        else if (c >= 97 && c <= 122) hasLowerCase = true;
+
+                        if (hasUpperCase && hasLowerCase) break;
+                    }
+                    if(hasUpperCase && hasLowerCase)
+                    password = value;
+                }
+            }
+        }
+
+
+        public bool HasDigit(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if(char.IsDigit(line[i])) return true;
+               
+            }
+            return false;
+        }
+
+        public bool HasUpper(string line)
+        {
+            foreach (var item in line)
+            {
+                if (item >= 65 && item <= 90) 
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+        public bool HasLower(string line)
+        {
+            foreach (var item in line)
+            {
+                if (item >= 97 && item <= 122) return true;
+            }
+
+            return false;
+        }
+
+    }
+}
